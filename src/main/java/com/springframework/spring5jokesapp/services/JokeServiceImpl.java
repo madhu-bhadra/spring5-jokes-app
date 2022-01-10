add6cfe98ec50614eb021eda5f285639bec7f7f4
madhu <madhu.bhadra@gmail.com>
2022-01-10 13:26:18 -0500

    Created Service Layer. Closes #3

---
+++ src/main/java/com/springframework/spring5jokesapp/services/JokeServiceImpl.java
@@ -0,0 +1,17 @@
+package com.springframework.spring5jokesapp.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
+public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
+    }
+
+    @Override
+    public String getJokes() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
